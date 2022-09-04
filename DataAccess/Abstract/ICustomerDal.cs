4f1e234bbfb4735353fec80a18b122dae2bb534b
ibrahim seyman <seyman12345@gmail.com>
2022-09-04 18:11:54 +0300

    04/09/2022

--- DataAccess/Abstract/ICustomerDal.cs
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal : IEntityRepository<Product>
+    public interface ICustomerDal : IEntityRepository<Customer>
     {
-        
     }
 }
